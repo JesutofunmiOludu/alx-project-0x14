382314d77ef910dfe40c450a1b9fedd046c780b1
LaughterOludu <99695248+LaughterOludu@users.noreply.github.com>
2025-12-08 00:05:56 +0100

    Tas 1 update

---
+++ alx-movie-app/components/commons/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button>
+      Button Component
+    </button>
+  );
+};
+
+export default Button;
---
+++ alx-movie-app/components/layout/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button>
+      Layout Button Component
+    </button>
+  );
+};
+
+export default Button;
