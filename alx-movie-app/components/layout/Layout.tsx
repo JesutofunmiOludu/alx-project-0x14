d47ad0ae787b96a0d88f07e18c780efaa647470d
LaughterOludu <99695248+LaughterOludu@users.noreply.github.com>
2025-12-08 00:40:28 +0100

    task 2

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,21 +1,20 @@
 import React, { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { ComponentProps } from "@/interfaces";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Header />
-      <main className="flex-grow container mx-auto p-4">
-        {children}
-      </main>
+      <main className="min-h-screen">{children}</main>
       <Footer />
-    </div>
-  );
-};
+    </>
+  )
+}
 
 export default Layout;
