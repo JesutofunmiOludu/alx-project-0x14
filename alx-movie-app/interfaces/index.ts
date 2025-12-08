c29260aee3aa0cd5bfbba4c1cd6d1fe8d608d822
LaughterOludu <99695248+LaughterOludu@users.noreply.github.com>
2025-12-08 01:18:04 +0100

    task 3

--- alx-movie-app/interfaces/index.ts
+++ alx-movie-app/interfaces/index.ts
@@ -8,3 +8,40 @@ export interface ButtonProps {
   title: string
   action?: () => void
 }
+
+import { ReactNode } from "react";
+
+export interface ComponentProps {
+  children: ReactNode
+}
+
+export interface ButtonProps {
+  title: string
+  action?: () => void
+}
+
+export interface MovieProps {
+  id?: string
+  posterImage: string
+  releaseYear: string
+  title: string
+}
+
+interface PrimaryImage {
+  url: string
+}
+
+interface TitleText {
+  text: string
+}
+
+interface ReleaseYear {
+  year: string
+}
+
+export interface MoviesProps {
+  id: string
+  primaryImage: PrimaryImage
+  titleText: TitleText
+  releaseYear: ReleaseYear
+}
